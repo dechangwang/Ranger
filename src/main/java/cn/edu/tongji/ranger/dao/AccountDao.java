4ee4eb65febb92e62bf7150ca49d337b056f656e
mirrordust <liaoshanhe@gmail.com>
2016-04-16 10:53:25 +0800

    try on restful 1

--- src/main/java/cn/edu/tongji/ranger/dao/AccountDao.java
+++ src/main/java/cn/edu/tongji/ranger/dao/AccountDao.java
@@ -2,7 +2,18 @@ package cn.edu.tongji.ranger.dao;
 
 import cn.edu.tongji.ranger.model.Account;
 import com.googlecode.genericdao.dao.hibernate.GenericDAO;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public interface AccountDao extends GenericDAO <Account, Long> {
+import java.util.List;
 
+public interface AccountDao {
+    public Integer create(Account account);
+    public Integer update(Account account);
+    public List<Account> findByName(String name);
+
+//    public void
 }
+/*public interface AccountDao extends GenericDAO <Account, Long> {
+
+}*/
