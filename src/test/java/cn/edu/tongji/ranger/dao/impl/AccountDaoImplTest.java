7cf4933a061e538fe48cf9072dbadbac1039f929
Dai Dongyang <daidongyang12@gmail.com>
2016-05-05 18:38:05 +0800

    commit before checkout branch

---
+++ src/test/java/cn/edu/tongji/ranger/dao/impl/AccountDaoImplTest.java
@@ -0,0 +1,21 @@
+package cn.edu.tongji.ranger.dao.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by daidongyang on 5/5/16.
+ */
+public class AccountDaoImplTest {
+
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void findByName() throws Exception {
+
+    }
+}
