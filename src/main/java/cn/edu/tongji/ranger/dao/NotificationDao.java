9839877ae8564772558f8df92d60ddcc2bacf83c
mirrordust <liaoshanhe@gmail.com>
2016-06-05 19:43:20 +0800

    notification details

--- src/main/java/cn/edu/tongji/ranger/dao/NotificationDao.java
+++ src/main/java/cn/edu/tongji/ranger/dao/NotificationDao.java
@@ -8,6 +8,18 @@ public interface NotificationDao extends BaseDao<Notification> {
 
     public Notification findById(Long id);
 
+    public List findUnreadNotificationsByAngencyId(Long id);
+
+    public List findUnreadNotificationsByAngencyId(Long id, int page);
+
+    public List findReadNotificationsByAngencyId(Long id);
+
+    public List findReadNotificationsByAngencyId(Long id, int page);
+
     public List findAllNotificationsByAngencyId(Long id);
+
+    public List findAllNotificationsByAngencyId(Long id, int page);
+
+    public Integer getPageNumber(Long aid, String type);
 
 }
