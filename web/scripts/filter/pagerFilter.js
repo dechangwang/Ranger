41af0b2b59906976b295fb054373894e99ee2ebf
Dai Dongyang <daidongyang12@gmail.com>
2016-06-20 00:58:17 +0800

    收藏分页实现

--- web/scripts/filter/pagerFilter.js
+++ web/scripts/filter/pagerFilter.js
@@ -5,6 +5,9 @@
 rangerApp.filter('pagerFilter', function(){
     var filter = function(inputArray, currentPage, pageSize){
         var array = [];
+        console.log(inputArray);
+        console.log(currentPage);
+        console.log(pageSize);
         for(var i = 0; i < inputArray.length; i++){
             if(i > (currentPage-1)*pageSize -1 && i < currentPage*pageSize){
                 array.push(inputArray[i]);
