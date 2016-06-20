22d8b9576bede77112c9773b09158a36f7948e38
思妙林 <linsm8968@126.com>
2016-06-20 10:35:13 +0800

    no message

---
+++ src/test/java/cn/edu/tongji/ranger/dao/AngencyDaoTest.java
@@ -0,0 +1,16 @@
+package cn.edu.tongji.ranger.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sophie on 16/6/20.
+ */
+public class AngencyDaoTest {
+    @Test
+    public void findById() throws Exception {
+
+    }
+
+}
